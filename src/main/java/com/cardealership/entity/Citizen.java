24a97711f05ea5a2845155ef6611a2c22dd5a965
EvangelosBais <baggosbais97@gmail.com>
2025-01-03 19:20:52 +0200

    add citizen and dealership entities

--- src/main/java/com/cardealership/entity/Citizen.java
+++ src/main/java/com/cardealership/entity/Citizen.java
@@ -2,4 +2,37 @@ package com.cardealership.entity;
 
 public class Citizen {
 
+    private int vat;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Citizen(int vat, String firstName, String lastName, String email, String password) {
+        this.vat = vat;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getVat() {
+        return vat;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/com/cardealership/entity/Dealership.java
+++ src/main/java/com/cardealership/entity/Dealership.java
@@ -2,4 +2,31 @@ package com.cardealership.entity;
 
 public class Dealership {
 
+    private int vat;
+    private String brandName;
+    private String owner;
+    private String password;
+
+    public Dealership(int vat, String brandName, String owner, String password) {
+        this.vat = vat;
+        this.brandName = brandName;
+        this.owner = owner;
+        this.password = password;
+    }
+
+    public int getVat() {
+        return vat;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
